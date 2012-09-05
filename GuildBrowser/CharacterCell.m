1adcf1669b030a31164f9aa14e13b95bb674b749
Charlie Fulton <charlie@fulton>
2012-09-05 18:01:27 -0400

    initil checkin

---
+++ GuildBrowser/CharacterCell.m
@@ -0,0 +1,30 @@
+#import "CharacterCell.h"
+#import "Character.h"
+#import "UIImageView+AFNetworking.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation CharacterCell
+
+-(id)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+    }
+    return self;
+}
+
+
+-(void)configureCellWithCharacter:(Character *)selectedCharacter
+{
+    self.character = selectedCharacter;
+    
+    _name.text = _character.name;
+    _levelRace.text = [NSString stringWithFormat:@"%@ %@",_character.level,_character.race];
+    _className.text = _character.classType;
+    _realm.text = _character.realm;
+    _achievementPoints.text = [_character.achievementPoints stringValue];
+    _guildRank.text = [_character.guildRank stringValue];
+    [_thumbNail setImageWithURL:[NSURL URLWithString:[_character thumbnailUrl]]];
+}
+
+@end
